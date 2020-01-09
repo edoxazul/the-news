20769bb0ad6ccd323762ee3c2ca33a2102ef5723
Eduardo <edualvsald@hotmail.com>
2020-01-09 17:42:48 -0300

    feat(activities) : Added Refresh , and Adapter connection with Recyclerview

--- app/src/main/java/cl/ucn/disc/dsm/thenews/activities/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/thenews/activities/MainActivity.java
@@ -1,9 +1,19 @@
 package cl.ucn.disc.dsm.thenews.activities;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import cl.ucn.disc.dsm.thenews.R;
+import cl.ucn.disc.dsm.thenews.activities.adapters.NoticiaAdapter;
 import cl.ucn.disc.dsm.thenews.databinding.ActivityMainBinding;
+import cl.ucn.disc.dsm.thenews.model.Noticia;
+import cl.ucn.disc.dsm.thenews.services.NoticiaService;
+import cl.ucn.disc.dsm.thenews.services.newsapi.NewsApiNoticiaService;
+import es.dmoral.toasty.Toasty;
+import java.util.List;
+import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,13 +23,25 @@ public class MainActivity extends AppCompatActivity {
   /**
    * The Logger
    */
+
   private static final Logger log = LoggerFactory.getLogger(MainActivity.class);
+
+  /**
+   * The Adapter
+   */
+  private NoticiaAdapter noticiaAdapter;
+
+  /**
+   * The NoticiaService
+   */
+  private NoticiaService noticiaService;
+
 
   /**
    * @param savedInstanceState to use.
    */
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate (Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
@@ -36,10 +58,89 @@ public class MainActivity extends AppCompatActivity {
 
     // The refresh
     {
-      binding.swlRefresh.setOnRefreshListener(() -> {
+      binding.swlRefresh.setOnRefreshListener(( ) -> {
         log.debug("Refreshing ..");
       });
     }
+
+    // The Adapter + RecyclerView
+    {
+      // The Adapter
+      this.noticiaAdapter = new NoticiaAdapter();
+
+      // The Adapter
+      binding.rvNoticias.setAdapter(this.noticiaAdapter);
+
+      // The layout (ListView)
+      binding.rvNoticias.setLayoutManager(new LinearLayoutManager(this));
+
+      // The separator (line)
+      binding.rvNoticias
+          .addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+    }
+
+    // The NoticiaService
+    this.noticiaService = new NewsApiNoticiaService();
+
+    // The refresh
+    {
+      binding.swlRefresh.setOnRefreshListener(( ) -> {
+        log.debug("Refreshing ..");
+
+        // Execute in background ..
+        AsyncTask.execute(( ) -> {
+
+          // How much time do we need?
+          final StopWatch stopWatch = StopWatch.createStarted();
+
+          try {
+
+            // 1. Get the List from NewsApi (in background)
+            final List<Noticia> noticias = this.noticiaService.getNoticias(50);
+
+            // (in UI)
+            this.runOnUiThread(( ) -> {
+
+              // 2. Set in the adapter (
+              this.noticiaAdapter.setNoticias(noticias);
+
+              // 3. Show a Toast!
+              Toasty.success(this, "Done: " + stopWatch, Toasty.LENGTH_SHORT).show();
+
+            });
+
+          } catch (Exception ex) {
+
+            log.error("Error", ex);
+
+            // (in UI)
+            this.runOnUiThread(( ) -> {
+
+              // Build the message
+              final StringBuffer sb = new StringBuffer("Error: ");
+              sb.append(ex.getMessage());
+              if (ex.getCause() != null) {
+                sb.append(", ");
+                sb.append(ex.getCause().getMessage());
+              }
+
+              // 3. Show the Toast!
+              Toasty.success(this, sb.toString(), Toasty.LENGTH_LONG).show();
+
+            });
+
+          } finally {
+
+            // 4. Hide the spinning circle
+            binding.swlRefresh.setRefreshing(false);
+
+          }
+
+        });
+
+      });
+    }
+
 
   }
 
