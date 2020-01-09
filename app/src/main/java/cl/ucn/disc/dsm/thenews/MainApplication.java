8510d9e8b0e2f00a777ea6f79e78ace8f642abf2
Eduardo <edualvsald@hotmail.com>
2020-01-09 17:21:30 -0300

    feat : Added Night Mode

--- app/src/main/java/cl/ucn/disc/dsm/thenews/MainApplication.java
+++ app/src/main/java/cl/ucn/disc/dsm/thenews/MainApplication.java
@@ -1,14 +1,38 @@
+/*
+ * Copyright 2020
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package cl.ucn.disc.dsm.thenews;
 
 import android.app.Application;
 import androidx.appcompat.app.AppCompatDelegate;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.jakewharton.threetenabp.AndroidThreeTen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * The Main Application
  *
- * @author Diego Urrutia-Astorga.
+ * @author Eduardo Alvarez S.
  */
 public class MainApplication extends Application {
 
@@ -23,12 +47,22 @@ public class MainApplication extends Application {
    */
   @Override
   public void onCreate() {
+
     super.onCreate();
+    log.debug("Initializing ..");
 
     // Day and Night support
-    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+
+    // Facebook fresco
+    Fresco.initialize(this);
+    AndroidThreeTen.init(this);
 
     log.debug("Initializing: Done.");
   }
+
+
+
+
 
 }
