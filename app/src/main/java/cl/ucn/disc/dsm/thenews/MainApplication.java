dd224b6df17f54b3b9eb76a16e214a2ea3612474
Eduardo <edualvsald@hotmail.com>
2020-01-17 09:19:03 -0300

    feat(adapters/NoticiaAdapter): Added PopUpImage

--- app/src/main/java/cl/ucn/disc/dsm/thenews/MainApplication.java
+++ app/src/main/java/cl/ucn/disc/dsm/thenews/MainApplication.java
@@ -17,8 +17,13 @@
 package cl.ucn.disc.dsm.thenews;
 
 import android.app.Application;
+import android.os.StrictMode;
+import android.os.StrictMode.ThreadPolicy;
+import android.os.StrictMode.VmPolicy;
 import androidx.appcompat.app.AppCompatDelegate;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.decoder.SimpleProgressiveJpegConfig;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,16 +48,46 @@ public class MainApplication extends Application {
    */
   @Override
   public void onCreate() {
-
     super.onCreate();
     log.debug("Initializing ..");
 
+
+    // Fresco configuration for large images
+    ImagePipelineConfig config = ImagePipelineConfig.newBuilder(this)
+        .setProgressiveJpegConfig(new SimpleProgressiveJpegConfig())
+        .setResizeAndRotateEnabledForNetwork(true)
+        .setDownsampleEnabled(true)
+        .build();
+
+    // Fresco initialization
+    Fresco.initialize(this, config);
+
     // Day and Night support
     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-
-    // Facebook fresco
-    Fresco.initialize(this);
     AndroidThreeTen.init(this);
+
+    if (BuildConfig.DEBUG) {
+
+      StrictMode.setThreadPolicy(new ThreadPolicy.Builder()
+          .detectDiskReads()
+          .detectDiskWrites()
+          .detectNetwork()
+          .penaltyLog()
+          .penaltyFlashScreen()
+          .build());
+
+      StrictMode.setVmPolicy(new VmPolicy.Builder()
+          .detectLeakedSqlLiteObjects()
+          .detectLeakedClosableObjects()
+          .detectLeakedRegistrationObjects()
+          .detectActivityLeaks()
+          .detectCleartextNetwork()
+          .penaltyLog()
+          .build());
+
+    }
+
+
 
     log.debug("Initializing: Done.");
   }
--- app/src/main/java/cl/ucn/disc/dsm/thenews/activities/adapters/NoticiaAdapter.java
+++ app/src/main/java/cl/ucn/disc/dsm/thenews/activities/adapters/NoticiaAdapter.java
@@ -19,14 +19,23 @@
 
 package cl.ucn.disc.dsm.thenews.activities.adapters;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
+import cl.ucn.disc.dsm.thenews.R;
+import cl.ucn.disc.dsm.thenews.databinding.PopupImageBinding;
 import cl.ucn.disc.dsm.thenews.databinding.RowNoticiaBinding;
 import cl.ucn.disc.dsm.thenews.model.Noticia;
 import java.util.ArrayList;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * ViewHolder Pattern.
@@ -34,6 +43,8 @@ import java.util.List;
  * @author Eduardo Alvarez S.
  */
 public class NoticiaAdapter extends RecyclerView.Adapter<NoticiaViewHolder> {
+
+  private static final Logger log = LoggerFactory.getLogger(NoticiaAdapter.class);
 
   /**
    * The List of Noticias.
@@ -44,7 +55,12 @@ public class NoticiaAdapter extends RecyclerView.Adapter<NoticiaViewHolder> {
    * The Constructor.
    */
   public NoticiaAdapter() {
+
+    // Empty list
     this.theNoticias = new ArrayList<>();
+
+    // Each Noticia has unique id
+    this.setHasStableIds(true);
   }
 
   /**
@@ -64,11 +80,74 @@ public class NoticiaAdapter extends RecyclerView.Adapter<NoticiaViewHolder> {
   /**
    * Called when RecyclerView needs a newViewHolder of the given type to represent an item.
    */
+  @NotNull
   @Override
   public NoticiaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
+    // The inflater
     final LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-    return new NoticiaViewHolder(RowNoticiaBinding.inflate(layoutInflater,parent,false));
+
+    // The row of noticia
+    final RowNoticiaBinding rowNoticiaBinding = RowNoticiaBinding.inflate(
+        layoutInflater,
+        parent,
+        false
+    );
+
+    // The NoticiaViewHolder
+    final NoticiaViewHolder noticiaViewHolder = new NoticiaViewHolder(rowNoticiaBinding);
+
+    // Click over the image
+    rowNoticiaBinding.sdvFoto.setOnClickListener(view -> {
+
+      // The position
+      final int position = noticiaViewHolder.getAdapterPosition();
+
+      // The id
+      final long id = noticiaViewHolder.getItemId();
+      log.debug("Click! position: {}, id: {}.", position, Long.toHexString(id));
+
+      // Noticia to show
+      final Noticia noticia = this.theNoticias.get(position);
+
+      // Nothing to do
+      if (noticia.getUrlFoto() == null) {
+        return;
+      }
+
+      // Popup the image
+      this.showImagePopup(noticia, layoutInflater, parent.getContext());
+
+    });
+
+    // Click in the row
+    rowNoticiaBinding.getRoot().setOnClickListener(view -> {
+
+      // The position
+      final int position = noticiaViewHolder.getAdapterPosition();
+
+      // The id
+      final long id = noticiaViewHolder.getItemId();
+      log.debug("Click! position: {}, id: {}.", position, Long.toHexString(id));
+
+      // Noticia to show
+      final Noticia noticia = this.theNoticias.get(position);
+
+      log.debug("Link: {}.", noticia.getUrl());
+      if (noticia.getUrl() != null) {
+
+        // Open the browser
+        parent.getContext().startActivity(
+            new Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(noticia.getUrl())
+            )
+        );
+      }
+
+    });
+
+    return noticiaViewHolder;
 
   }
 
@@ -88,5 +167,37 @@ public class NoticiaAdapter extends RecyclerView.Adapter<NoticiaViewHolder> {
   public int getItemCount() {
     return this.theNoticias.size();
   }
+  /**
+   * Return the stable ID for the item at position.
+   */
+  @Override
+  public long getItemId(int position) {
+    return this.theNoticias.get(position).id;
+  }
+
+
+  /**
+   * Show a image popup with the url.
+   *
+   * @param noticia  to show.
+   * @param inflater used to inflate the popup.
+   * @param context  used to build the dialog.
+   */
+  private void showImagePopup(final Noticia noticia, final LayoutInflater inflater, final Context context) {
+
+    // The popupimage
+    final PopupImageBinding popupImageBinding = PopupImageBinding.inflate(inflater);
+
+    // The URL of the photo
+    popupImageBinding.pdvFoto.setPhotoUri(Uri.parse(noticia.getUrlFoto()));
+
+    // The Dialog
+    final Dialog dialog = new Dialog(context, R.style.PopupDialog);
+    dialog.setContentView(popupImageBinding.getRoot());
+    dialog.show();
+
+  }
+
 
 }
+
